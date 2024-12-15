c67d8fbb20dc1d1a4b6b568f9606518724cf3c4f
Katrin889 <kakamitreva@gmail.com>
2024-12-15 19:17:13 +0200

    Add files via upload

---
+++ zad 51/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_51
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+            double Z = 0;
+            while (n > 0)
+            {
+                int r = n % 2;
+                n = n / 2;
+                Z = Z + Math.Pow(10, i) * r;
+                i++;
+            }
+            Console.WriteLine(Z);
+        }
+    }
+}
